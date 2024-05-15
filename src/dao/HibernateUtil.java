489c84f4bb1b2c9fa9d3dd18b8b720195c706515
ArturoEduartes <ArturoEduartes@users.noreply.github.com>
2024-05-15 12:55:05 -0300

    configurando bd - in progress

---
+++ src/dao/HibernateUtil.java
@@ -0,0 +1,33 @@
+/* Consultas:
+ * ¿ se debe agregar un constructor privado para ocultar el constructor publico implicito?
+ */
+
+package dao;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateUtil {
+	private static SessionFactory sessionFactory;
+
+	public static SessionFactory getSessionFactory() {
+		try {
+			if (sessionFactory == null) {
+				StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+						.configure()
+						.build();
+				Metadata metaData = new MetadataSources().buildMetadata(standardRegistry);
+				sessionFactory = metaData.buildSessionFactory();
+			}
+		} catch (HibernateException he) {
+			System.err.println("ERROR en la inicialización de la SessionFactory: " + he);
+			throw new ExceptionInInitializerError(he);
+		}
+
+		return sessionFactory;
+	}
+}
---
+++ src/test/TestHBM.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import dao.HibernateUtil;
+
+public class TestHBM {
+
+	public static void main(String[] args) {
+
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction tx = session.beginTransaction();
+		System.out.println(tx.getStatus());
+		session.close();
+		System.out.println("Ok connection");
+		
+
+	}
+
+}
